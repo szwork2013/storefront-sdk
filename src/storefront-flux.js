b3eb479e87637e310255cf8f6e0921f625634946
Breno Calazans <breno@vtex.com.br>
2015-07-13 19:02:42 -0300

    Replace modules

--- src/storefront-flux.js
+++ src/storefront-flux.js
@@ -1,14 +1,14 @@
 // Bundle default actions and stores
 /*eslint-disable no-unused-vars*/
 import flux from './flux.js'; // must be imported first
-import CartActions from './cart/actions.js';
-import SearchActions from './search/actions.js';
-import ProductActions from './product/actions.js';
-import ComponentActions from './component/actions.js';
-import EditorActions from './editor/actions.js';
-import CartStore from './cart/store.js';
-import SearchStore from './search/store.js';
-import ProductStore from './product/store.js';
-import ShopStore from './shop/store.js';
-import ComponentStore from './component/store.js';
-import EditorStore from './editor/store.js';
+import CartActions from './modules/cart/actions.js';
+import SearchActions from './modules/search/actions.js';
+import ProductActions from './modules/product/actions.js';
+import ComponentActions from './modules/component/actions.js';
+import EditorActions from './modules/editor/actions.js';
+import CartStore from './modules/cart/store.js';
+import SearchStore from './modules/search/store.js';
+import ProductStore from './modules/product/store.js';
+import ShopStore from './modules/shop/store.js';
+import ComponentStore from './modules/component/store.js';
+import EditorStore from './modules/editor/store.js';
