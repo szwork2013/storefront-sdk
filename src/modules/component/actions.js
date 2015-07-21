77d8b94209bec57aeb929b9bdc8b9caa7bf7bece
Breno Calazans <breno@vtex.com.br>
2015-07-21 11:42:40 -0300

    Create service for Storefront

--- src/modules/component/actions.js
+++ src/modules/component/actions.js
@@ -1,28 +1,14 @@
 import storefront from 'storefront';
-import $ from 'jQuery';
+import Storefront from 'services/Storefront';
+
+let storefrontService = new Storefront();
 
 class ComponentActions {
   saveSettings({accountName, route, component, id, settings}) {
-    const url = `http://api.beta.vtex.com/${accountName}/storefront/components/${route}/${id}`;
-    const data = {
-      'component': component,
-      'settings': settings
-    };
-    const token = storefront.flux.stores.ShopStore.getState().get('token');
-    const authorization = `token ${token}`;
 
-    $.ajax({
-      url: url,
-      method: 'PUT',
-      contentType: 'application/json; charset=utf-8',
-      dataType: 'json',
-      headers: {
-        'Accept': 'application/vnd.vtex.storefront.v0+json',
-        'Authorization': authorization
-      },
-      data: JSON.stringify(data)
-    }).done(() => this.actions.saveSettingsSuccess({route, id, settings}))
-    .fail(this.actions.saveSettingsError);
+    storefrontService.saveComponentSettings({accountName, route, component, id, settings})
+      .done(() => this.actions.saveSettingsSuccess({route, id, settings}))
+      .fail(this.actions.saveSettingsError);
 
     return arguments[0];
   }
---
+++ src/services/Storefront.js
@@ -0,0 +1,38 @@
+import jQuery from 'jQuery';
+import _extend from 'lodash/object/assign';
+import constants from './constants.js';
+
+class Storefront {
+  constructor() {
+    let token = ('; ' + document.cookie).split('; VtexIdclientAutCookie=').pop().split(';').shift();
+
+    this.defaultAjax = {
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      headers: {
+        'Accept': 'application/vnd.vtex.storefront.v0+json',
+        'Authorization': `token ${token}`
+      }
+    };
+  }
+
+  saveComponentSettings({accountName, route, component, id, settings}) {
+    const url = `${constants.BASE_URL}${accountName}/storefront/components/${route}/${id}`;
+
+    const data = {
+      'component': component,
+      'settings': settings
+    };
+
+    let props = {
+      url: url,
+      method: 'PUT',
+      data: JSON.stringify(data)
+    };
+
+    return jQuery.ajax(_extend({}, this.defaultAjax, props));
+  }
+
+}
+
+export default Storefront;
