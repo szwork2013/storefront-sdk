25daacd520138a4f181b2243bcff51145551debd
Breno Calazans <breno@vtex.com.br>
2015-08-18 16:18:34 -0300

    Fix Search service

--- src/services/Search.js
+++ src/services/Search.js
@@ -1,23 +1,22 @@
-import _omit from 'lodash/object/omit';
-import StorefrontConstants from 'constants/StorefrontConstants';
 import axios from 'axios';
 
 class Search {
-  static products(params) {
-    const url = StorefrontConstants.BASE_URL + params.accountName + '/products/' + (params.product || '');
+  static productResource = '/_resources/product@vtex.storefront-sdk/'
+  static productsResource = '/_resources/products@vtex.storefront-sdk/'
 
-    return axios.get(url, {params: _omit(params, ['accountName', 'product', 'id'])});
+  static products(params) {
+    return axios.get(this.productResource, { params: params });
   }
 
   static facets(params) {
-    const url = StorefrontConstants.BASE_URL + params.accountName + '/facets';
+    const url = this.productsResource + '/facets';
 
-    return axios.get(url, {params: _omit(params, ['accountName'])});
+    return axios.get(url, { params: params });
   }
 
   static categories(params) {
     const categoryId = params.category ? params.category : '';
-    const url = StorefrontConstants.BASE_URL + params.accountName + '/categories/' + categoryId;
+    const url = this.productsResource + '/categories/' + categoryId;
 
     return axios.get(url);
   }
