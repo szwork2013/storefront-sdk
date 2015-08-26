e5ac1ac47b9454fa5038202885746cd2a8061637
Breno Calazans <breno@vtex.com.br>
2015-08-26 14:56:51 -0300

    Add App component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import sdk from 'expose?storefront.sdk!./StorefrontSDK';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import actions from './actions';
 import stores from './stores';
+import App from './App';
 import { map } from 'lodash';
 
 // Needed for onTouchTap
@@ -13,3 +14,11 @@ injectTapEventPlugin();
 // Register all actions and stores
 map(actions, (action) => sdk.dispatcher.addActions(action.name, action.obj));
 map(stores, (store) => sdk.dispatcher.addStore(store.name, store.obj, sdk.dispatcher));
+
+// Register App component
+let component = {
+  name: 'App',
+  constructor: App
+};
+
+sdk.dispatcher.actions.ComponentActions.register(component);
