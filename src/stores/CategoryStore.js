b3a8ab4217437c349d2a38785963415993e2f22c
Thor Amorim <thoramorim@gmail.com>
2015-08-31 17:14:00 -0300

    Add CategoryStore

---
+++ src/stores/CategoryStore.js
@@ -0,0 +1,60 @@
+import Immutable from 'immutable';
+import immutable from 'alt/utils/ImmutableUtil';
+import { isArray, values, flatten } from 'lodash';
+
+function addCategories(state, categories) {
+  if (!isArray(categories)) {
+    categories = [categories];
+  }
+
+  let newCategories = state.withMutations(map => {
+    categories.forEach( category => map.set(category.slug, category) );
+  });
+
+  return newCategories;
+}
+
+function getDataFromResources(state) {
+  let categories = [];
+  let resources = window.storefront.currentRoute.resources;
+  categories = categories.concat(flatten(values(resources['categories@vtex.storefront-sdk'])));
+
+  return addCategories(state, categories);
+}
+
+@immutable
+class CategoryStore {
+  constructor(dispatcher) {
+    this.bindActions(dispatcher.actions.CategoryActions);
+
+    this.state = getDataFromResources(Immutable.Map());
+
+    this.exportPublicMethods({
+      getCategories: this.getCategories
+    });
+  }
+
+  getCategories(categories) {
+    let result = [];
+
+    for (var i = 0; i < categories.length; i++) {
+      let category = this.state.get(categories[i]);
+      if (category) {
+        result.push(category);
+      }
+    }
+
+    return result;
+  }
+
+  onRequestCategoriesSuccess(categories) {
+    this.setState(addCategories(this.state, product));
+  }
+
+  onRequestCategoriesFail(error) {
+    this.setState(this.state.set('error', error));
+  }
+
+}
+
+export default CategoryStore;
