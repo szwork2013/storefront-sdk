6df9591a887c547ad33561851a4dcc7b9059a159
Thor Amorim <thoramorim@gmail.com>
2015-08-31 17:13:29 -0300

    Add CategoryActions

---
+++ src/actions/CategoryActions.js
@@ -0,0 +1,12 @@
+class CategoryActions {
+  requestCategoriesSuccess(categories) {
+    return categories;
+  }
+
+  requestCategoriesFail(error) {
+    return error;
+  }
+
+}
+
+export default CategoryActions;
