7c2dd1f8141e20821a5587db168b0b2dfe12ff31
Breno Calazans <breno@vtex.com.br>
2015-09-24 13:40:31 -0300

    Use React Router 1.0.0
    Fix #11
    
    Please refer to React Router upgrade guide to update your code
    https://github.com/rackt/react-router/blob/master/UPGRADE_GUIDE.md

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { RouteHandler } from 'react-router';
 
 class App extends React.Component {
   render() {
     return (
-      <RouteHandler/>
+      <div>
+        {this.props.children}
+      </div>
     );
   }
 }
