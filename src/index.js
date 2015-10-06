e3c09f52717a6e25388b34e6b5552a44db4403b5
Breno Calazans <breno@vtex.com.br>
2015-10-06 15:40:57 -0300

    Lint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import sdk from 'expose?storefront.sdk!./StorefrontSDK';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import actions from './actions';
 import stores from './stores';
 import { map } from 'lodash-compat/collection';
@@ -8,6 +7,7 @@ import { map } from 'lodash-compat/collection';
 // Can go away when react 1.0 release
 // Check this repo:
 // https://github.com/zilverline/react-tap-event-plugin
+import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 // Register all actions and stores
