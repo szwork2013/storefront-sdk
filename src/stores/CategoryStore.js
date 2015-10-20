a090e0b9ae1e8b7878e253d9f05e7296db7e0edc
Thor Amorim <thoramorim@gmail.com>
2015-10-20 16:53:38 -0200

    Revert "Merge pull request #21"
    This reverts commit 6299430cdf5e179201f9e1a08df87ebfd1be2215, reversing
    changes made to bbfc44e28fbc4bdb0db1a052bc96b000b04f9c28.

--- src/stores/CategoryStore.js
+++ src/stores/CategoryStore.js
@@ -3,7 +3,6 @@ import immutable from 'alt/utils/ImmutableUtil';
 import { isArray } from 'lodash-compat/lang';
 import { values } from 'lodash-compat/object';
 import { flatten } from 'lodash-compat/array';
-import { rest } from 'lodash-compat/array';
 
 function addCategories(state, categories) {
   if (!isArray(categories)) {
@@ -11,9 +10,7 @@ function addCategories(state, categories) {
   }
 
   let newCategories = state.withMutations(map => {
-    categories.forEach((category) => {
-      map.set(category.slug, Immutable.fromJS(category));
-    });
+    categories.forEach( category => map.set(category.slug, category) );
   });
 
   return newCategories;
@@ -31,20 +28,6 @@ class CategoryStore {
   constructor(dispatcher) {
     this.bindActions(dispatcher.actions.CategoryActions);
     this.state = getDataFromResources(Immutable.Map());
-
-    this.exportPublicMethods({
-      getCategory: this.getCategory
-    });
-  }
-
-  // Returns a category given an array of hierarchical category slugs
-  getCategory(slugs, categories = this.state) {
-    let category = categories.filter(function(cat) {
-      return cat.get('slug') === slugs[0];
-    }).first();
-
-    return slugs.length === 1 ?
-      category : this.getCategory(rest(slugs), category.get('children'));
   }
 }
 
